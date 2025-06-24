03ffbb8c7fc210be1fe5c6a0ff8911cce66aaa03
chu0802 <r09922104@ntu.edu.tw>
2025-06-24 19:18:14 +0800

    <update>: add audio verifier

--- src/gptwrapper/response/audio_understanding.py
+++ src/gptwrapper/response/audio_understanding.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel
 
 class AudioUnderstandingResponse(BaseModel):
-    action: str
-    object: str
-    material: str
+    answer: str
     justification: str
 
 class AudioRankingResponse(BaseModel):
