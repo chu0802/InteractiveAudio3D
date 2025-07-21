58ac464f0dfe730c2c072c8adc560a72cc780d8d
chu0802 <r09922104@ntu.edu.tw>
2025-07-21 17:58:25 +0800

    <update>: support customized criteria

--- dataset_creation.py
+++ dataset_creation.py
@@ -6,6 +6,19 @@ import shutil
 from collections import defaultdict
 
 
+CUSTOM_CRITERION = {
+    "action_then_others": lambda x: x["action"] * 10 + x["object"] + x["material"]
+}
+
+def get_scores(obj, criterion):
+    if criterion in obj:
+        return obj[criterion]
+    elif criterion in CUSTOM_CRITERION:
+        return CUSTOM_CRITERION[criterion](obj)
+    else:
+        raise ValueError(f"Criterion {criterion} not found")
+    
+
 def main(args):
     data_dir = args.dataset_dir / args.scene_name
 
@@ -36,14 +49,14 @@ def main(args):
         filtered_data = {}
 
         for action, action_data in catagorized_data.items():
-            overall_scores = [entry['scores']['overall'] for entry in action_data.values()]
+            overall_scores = [get_scores(entry['scores'], args.criterion) for entry in action_data.values()]
 
             filter_threshold = np.percentile(overall_scores, args.filter_threshold)
             
-            if args.mode == "gt":
-                new_data = {k: v for k, v in action_data.items() if v['scores']['overall'] >= filter_threshold}
+            if args.mode == "pos":
+                new_data = {k: v for k, v in action_data.items() if get_scores(v['scores'], args.criterion) >= filter_threshold}
             else:
-                new_data = {k: v for k, v in action_data.items() if v['scores']['overall'] <= filter_threshold}
+                new_data = {k: v for k, v in action_data.items() if get_scores(v['scores'], args.criterion) <= filter_threshold}
 
             filtered_data.update(new_data)
 
@@ -75,10 +88,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dataset_dir", type=Path, default=Path("output"))
     parser.add_argument("-n", "--scene_name", type=str, default="0118_bathroom")
-    parser.add_argument("-o", "--output_dir", type=Path, default=Path("positive_samples"))
     parser.add_argument("-f", "--filter_threshold", type=int, default=95)
     parser.add_argument("-s", "--statistics", action="store_true", default=False)
-    parser.add_argument("-m", "--mode", type=str, default="gt", choices=["gt", "lt"])
+    parser.add_argument("-m", "--mode", type=str, default="pos", choices=["pos", "neg"])
     parser.add_argument("-t", "--target_obj", type=str, default=None)
+    parser.add_argument("-c", "--criterion", type=str, default="overall")
     args = parser.parse_args()
+    
+    args.output_dir = Path(f"{args.mode}_{args.criterion}_{args.filter_threshold}")
+
     main(args)
