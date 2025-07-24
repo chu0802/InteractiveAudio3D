55f389998ce4871381d0ba6b0f4088e87bb3731b
chu0802 <r09922104@ntu.edu.tw>
2025-07-24 16:01:19 +0800

    <update>: set the max epoch lower

--- src/stable_audio/loraw/train.py
+++ src/stable_audio/loraw/train.py
@@ -132,7 +132,7 @@ def main():
         callbacks=callbacks,
         logger=wandb_logger,
         log_every_n_steps=1,
-        max_epochs=500,
+        max_epochs=300,
         default_root_dir=args.save_dir,
         gradient_clip_val=args.gradient_clip_val,
         reload_dataloaders_every_n_epochs = 0,
